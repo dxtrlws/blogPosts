cf3ca3c9ab07fdeb4801b9af8cc64ecb58397b64
Dexter Lewis <dexterlewis@gmail.com>
2017-03-28 23:29:37 -0400

    initial commit

---
+++ blogRouter.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Admin on 3/28/2017.
+ */
+const express = require('express');
+const router = express.Router();
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Admin on 3/28/2017.
+ */
+const express = require('express');
+const router = express.Router();
+const morgon = require('morgan');
+const bodyParser = require('body-parser');
+const {blogPost} = require('./models');
+const blogRouter = require('./blogRouter');
+const app = express();
+
+app.use(morgon('common'));
+app.use('/blog-posts',  blogRouter);
+
+
+
+app.listen(process.env.PORT || 8080, () => {
+    console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+});
